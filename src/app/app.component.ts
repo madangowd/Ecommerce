b4034e887bcf34829ce048271c251d8c0b819149
madangowd <Madankumarparuchuri143@gmail.com>
2024-08-12 02:18:02 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CurrencyService } from './Service/currency.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ecommerce';
+ constructor(private currencyService : CurrencyService){}
+
+  selectCurrency:string=''
+
+  sendCurreny(event:string){
+    console.log(event);
+    this.currencyService.setCurrency(event);
+  }
+
+}
